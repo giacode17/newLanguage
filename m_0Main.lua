2531365875967d9feca9a349aee28aad65fcbc7a
Gia_Sun <jihyun.sun@icloud.com>
2023-11-20 07:38:27 -0800

    first

---
+++ m_0Main.lua
@@ -0,0 +1,11 @@
+
+local parser = require("parser")
+local compiler = require("compiler")
+local interpreter = require("interpreter")
+
+local input = io.read("a")
+local ast = parser.parse(input)
+local code = compiler.compile(ast)
+local result = interpreter.run(code, {}, {}, 0)
+--]print("result: " .. result)
+
